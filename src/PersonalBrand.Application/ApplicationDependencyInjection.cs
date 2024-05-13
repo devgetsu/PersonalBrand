27741ad5813952ae5cb3971d194c016b223ab1eb
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-13 12:22:29 +0500

    Add project files.

---
+++ src/PersonalBrand.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBrand.Application
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddApplicationDependencyInjection(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
---
+++ src/PersonalBrand.Domain/Entities/Auth/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBrand.Domain.Entities.Auth
+{
+    public class UserModel : IdentityUser<long>
+    {
+        public string FirstName { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTimeOffset ModifiedAt { get; set; }
+        public DateTimeOffset DeletedAt { get; set; }
+        public bool isDeleted { get; set; } = false;
+    }
+}
